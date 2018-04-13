4561e00171c93d500e587087340feb22c5b2a11f
matdombrock <matdombrock@gmail.com>
2018-04-13 01:27:46 -0700

    Minor Refactor

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import config
 import shortcodes
 import shutil
 import strings
-
 def Parameters():
 	for argument in sys.argv:
 		argument_split = argument.split("=")
@@ -30,16 +29,15 @@ def Parameters():
 		if argument_split[0] in "--auto-purge -a":
 			config.auto_purge = True
 			print(strings.arg_read["purge"])
-
 def CheckOutput():
 	if not os.path.isdir(config.output_location):
 		os.mkdir(config.output_location)
 	if os.listdir(config.output_location) != []:
 		if(config.auto_purge == True):
-			purge = "y" 
+			answer = "y" 
 		else:
-			purge = input(strings.check_output["warning"])
-		if(purge.lower()[0]=="y"):
+			answer = input(strings.check_output["warning"])
+		if(answer.lower()[0]=="y"):
 			print(strings.check_output["purging"]) 
 			for dirName, subdirList, fileList in os.walk(config.output_location):
 				for f in fileList:
@@ -48,10 +46,6 @@ def CheckOutput():
 					shutil.rmtree(os.path.join(dirName, d))
 		else:
 			print(strings.check_output["overwriting"])
-def MetaShortCodes(data):
-	data = data.replace("[[URL]]",config.url+"/")
-	return data
-
 def LoadCodes(defs):
 	defs_c = {}
 	for name, location in defs.items():
@@ -65,37 +59,36 @@ def LoadCodes(defs):
 			input(strings.load_codes["check"])
 			quit()
 	return defs_c
-
-
 def Build(rootDir,prefabs):
 	print(strings.build["look"]+rootDir)
 	for dirName, subdirList, fileList in os.walk(rootDir):
 		print(strings.build["found"]+'%s' % dirName)
 		for fname in fileList:
 			print('\t%s' % fname)
-			output_file = config.output_location+"/"+dirName+"/"+fname
+			output_file = config.output_location+dirName+"/"+fname
 			output_file = output_file.replace(config.template_location,"")#removes the template location from the output path
-			parts = os.path.split(output_file)
-			if not os.path.isdir(parts[0]):#makes sure we have a directory to write to
-				os.mkdir(parts[0])
+			path_parts = os.path.split(output_file)
+			if not os.path.isdir(path_parts[0]):#makes sure we have a directory to write to
+				os.mkdir(path_parts[0])
 			print(strings.build["output"]+output_file)
 			if(fname.split(".")[1].lower() in config.parse_types):
-				test = open(dirName+"/"+fname, 'r').read()
+				template_file = open(dirName+"/"+fname, 'r').read()
 				#meta-shortcodes
-				test = MetaShortCodes(test)
+				template_file = MetaShortCodes(template_file)
 				#end meta-shortcodes
 				for name, data in prefabs.items():
-					test = test.replace("{{"+name+"}}",data)
+					template_file = template_file.replace("{{"+name+"}}",data)
 				file = open(output_file, 'w')
-				file.write(test)
+				file.write(template_file)
 			else:
 				print(strings.build["no_parse"]+fname)
 				shutil.copyfile(dirName+"/"+fname, output_file)
-
+def MetaShortCodes(data):
+	data = data.replace("[[URL]]",config.url+"/")
+	return data
 def Start():
 	Parameters()
 	CheckOutput()
 	defs = LoadCodes(shortcodes.defs)
 	Build(config.template_location,defs)
-
 Start()
